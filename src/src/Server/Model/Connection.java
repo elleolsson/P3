ed560a2f2d6ef618105fad700d04c53f4c0091c2
elleolsson <eliasolsson5@gmail.com>
2024-02-29 09:16:58 +0100

    connection klass

--- src/src/Server/Model/Connection.java
+++ src/src/Server/Model/Connection.java
@@ -1,6 +1,11 @@
 package Server.Model;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Connection implements Runnable {
+    private ServerSocket serverSocket;
+    private Socket socket;
     @Override
     public void run() {
 
