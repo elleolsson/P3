2b306ca91b7e160338338e3e8991514813b147cb
elleolsson <eliasolsson5@gmail.com>
2024-02-29 09:45:46 +0100

    hej

--- src/src/Client/Model/User.java
+++ src/src/Client/Model/User.java
@@ -2,10 +2,13 @@ package Client.Model;
 
 import javax.swing.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class User implements Serializable { // Även användas i strömmar
     private String username;
     private ImageIcon image;
+    private ArrayList<Message> UserMessages = new ArrayList<>();
+    private ArrayList<User> contacts = new ArrayList<>();
 
     public User() {
 
--- src/src/Server/Controller/ConnectionHandler.java
+++ src/src/Server/Controller/ConnectionHandler.java
@@ -1,10 +1,5 @@
 package Server.Controller;
 
-import Client.Model.User;
-import Server.Model.Connection;
-
-import java.util.HashMap;
 
 public class ConnectionHandler {
-    private HashMap<User, Connection> connectionHashMap = new HashMap<>();
 }
