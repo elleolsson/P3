4e4933fb6a834a5164cfcd04a093ab6f615df1c8
elleolsson <eliasolsson5@gmail.com>
2024-02-29 09:34:45 +0100

    wtf händer

---
+++ src/src/Server/Controller/ConnectionHandler.java
@@ -0,0 +1,10 @@
+package Server.Controller;
+
+import Client.Model.User;
+import Server.Model.Connection;
+
+import java.util.HashMap;
+
+public class ConnectionHandler {
+    private HashMap<User, Connection> connectionHashMap = new HashMap<>();
+}
--- src/src/Server/Model/Connection.java
+++ src/src/Server/Model/Connection.java
@@ -2,10 +2,12 @@ package Server.Model;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Connection implements Runnable {
     private ServerSocket serverSocket;
     private Socket socket;
+
     @Override
     public void run() {
 
